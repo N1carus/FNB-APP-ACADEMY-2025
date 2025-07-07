1fa46230fe80c59793d37c394616275ced95bf33
N1carus <akieltitus22@gmail.com>
2025-07-07 19:59:13 +0200

    update

---
+++ Websites/WEEK 8/rectangle_calculator/rectangle.py
@@ -0,0 +1,10 @@
+import calculate
+
+length = float(input("Enter Length of the rectangle: "))
+width = float(input("Enter width of the rectangle: "))
+
+area = calculate.area(length, width)
+perimeter = calculate.perimeter(length, width)
+
+print(f"The area of the rectangle is: {area}")
+print(f"The perimeter of the rectangle is: {perimeter}")
